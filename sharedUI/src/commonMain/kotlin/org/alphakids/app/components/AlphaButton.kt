a23cb76e3be772b95b331565d9e0ee7677e87778
AndrewMechDev <andrewmechdev@gmail.com>
2026-07-02 10:08:41 -0500

    fix(components): add missing background import in AlphaButton

--- sharedUI/src/commonMain/kotlin/org/alphakids/app/components/AlphaButton.kt
+++ sharedUI/src/commonMain/kotlin/org/alphakids/app/components/AlphaButton.kt
@@ -22,6 +22,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.background
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.ui.graphics.graphicsLayer
